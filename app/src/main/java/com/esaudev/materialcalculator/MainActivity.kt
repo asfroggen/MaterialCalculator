799cf052f4d09913dcc1a4157e97d5b9b03c69a3
Esau <yosoypawn@hotmail.com>
2022-08-29 17:07:45 -0500

    Calculator Ui integration

--- app/src/main/java/com/esaudev/materialcalculator/MainActivity.kt
+++ app/src/main/java/com/esaudev/materialcalculator/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.esaudev.materialcalculator.presentation.CalculatorScreen
 import com.esaudev.materialcalculator.ui.theme.MaterialCalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,13 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialCalculatorTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                CalculatorScreen()
             }
         }
     }
